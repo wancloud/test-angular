ec8bde4e500ff160b8d7292e72561c095364945c
wancloud <wancloud@hotmail.com>
2024-09-23 02:13:53 +0800

    Commit made from VS Code Azure Static Web Apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test-angular';
+  title = 'this is my new angular app';
 }
