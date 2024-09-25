197d5b5d989ff02af8aa52533d54aaad9e8c12ab
wancloud <wancloud@hotmail.com>
2024-09-26 02:40:08 +0800

    update contact, hobby component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,14 @@ import { DescriptionComponent } from "./description/description.component";
 import { DescModel } from '../model/desc-model';
 import { SkillModel } from '../model/skill-model';
 import { SkillComponent } from "./skill/skill.component";
+import { HobbyComponent } from "./hobby/hobby.component";
+import { ContactComponent } from "./contact/contact.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AngularDemoComponent, CategoryComponent, TitleComponent, DescriptionComponent, SkillComponent],
+  imports: [CommonModule,RouterOutlet, AngularDemoComponent, CategoryComponent, TitleComponent, DescriptionComponent, SkillComponent, HobbyComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,11 +24,16 @@ export class AppComponent {
   descExp: DescModel[] = [];
   descEdu: DescModel[] = [];
   skills: SkillModel[] = [];
+  displayDemo: boolean = false;
 
   constructor() {
     this.aboutMe = myData.about;
     this.descEdu = myData.edu;
     this.descExp = myData.exp;
     this.skills = myData.skills;
+  }
+
+  onClick(event: MouseEvent) {
+    this.displayDemo = !this.displayDemo;
   }
 }
